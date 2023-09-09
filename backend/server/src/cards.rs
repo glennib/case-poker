7c4982450caa3dd2a1d91c13ae878d03ab7b875d
Glenn Bitar <glennbitar@gmail.com>
2023-09-09 09:43:57 +0200

    Remove unused next function from Rank

--- backend/server/src/cards.rs
+++ backend/server/src/cards.rs
@@ -24,24 +24,6 @@ pub enum Rank {
 }
 
 impl Rank {
-    pub fn next(self) -> Self {
-        match self {
-            Rank::Ace => Rank::Two,
-            Rank::Two => Rank::Three,
-            Rank::Three => Rank::Four,
-            Rank::Four => Rank::Five,
-            Rank::Five => Rank::Six,
-            Rank::Six => Rank::Seven,
-            Rank::Seven => Rank::Eight,
-            Rank::Eight => Rank::Nine,
-            Rank::Nine => Rank::Ten,
-            Rank::Ten => Rank::Jack,
-            Rank::Jack => Rank::Queen,
-            Rank::Queen => Rank::King,
-            Rank::King => Rank::Ace,
-        }
-    }
-
     pub fn numeric(&self) -> u8 {
         match self {
             Rank::Ace => { 1 }
